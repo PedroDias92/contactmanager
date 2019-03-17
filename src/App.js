19061b79d95e841f855347973ff169f370f21cdd
pedrodias92 <pedro.dias92@hotmail.com>
2019-03-17 23:26:34 +0000

    fix to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
