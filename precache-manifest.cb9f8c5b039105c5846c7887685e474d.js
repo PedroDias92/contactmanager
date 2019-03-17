be61d19c331cd691ce4b9614c2c8e2740c4ce34d
pedrodias92 <pedro.dias92@hotmail.com>
2019-03-17 23:32:04 +0000

    Updates

---
+++ precache-manifest.cb9f8c5b039105c5846c7887685e474d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b8774e2ddfe7415eb6",
+    "url": "/contactmanager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "82b8774e2ddfe7415eb6",
+    "url": "/contactmanager/static/js/main.b813d029.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "4b82a97f88b18d572000",
+    "url": "/contactmanager/static/css/2.00ae7f71.chunk.css"
+  },
+  {
+    "revision": "4b82a97f88b18d572000",
+    "url": "/contactmanager/static/js/2.ae7389ce.chunk.js"
+  },
+  {
+    "revision": "c8c7f7484971376494dfae937aadb449",
+    "url": "/contactmanager/index.html"
+  }
+];
